a51530514e21ffd4080ef03e0476d36053cf794b
VishnuVardhanBonala-web4U <rvardhancm08@gmail.com>
2024-08-05 19:03:18 +0530

    " Responsive Done "

--- src/components/NavbarV2/index.js
+++ src/components/NavbarV2/index.js
@@ -1,17 +1,18 @@
-/* eslint-disable no-unused-vars */
 import React, { useEffect, useState, useRef } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { Tab, Tabs } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu"; // Import the MenuIcon
 import Logo from "../../assets/images/logo.jpg";
 import avatar from "../../assets/svg/avatar.svg";
 import "./styles.css";
 import axios from "axios";
 import { useUser } from "../../pages/context/userContext";
 export const baseURL = "https://otamat.com/api";
+
 function NavbarV2() {
   const navigate = useNavigate();
   const location = useLocation();
-  const dropdownRef = useRef(null); // Add ref for dropdown
+  const dropdownRef = useRef(null);
 
   const handleLogout = () => {
     localStorage.removeItem("username");
@@ -23,6 +24,7 @@ function NavbarV2() {
   const [currentTab, setCurrentTab] = useState(0);
   const [isLogin, setIsLogin] = useState(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   const userName = localStorage.getItem("username");
   const { userData, setUserData } = useUser();
 
@@ -98,171 +100,131 @@ function NavbarV2() {
 
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light sticky-top" style={{
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        right: 0,
-        borderBottom: '1px solid lightgrey',
-        background: 'rgb(255 252 245)'
-      }}>
-        <div className="collapse navbar-collapse" style={{ marginLeft: '20px', display: 'flex', gap: '20px' }} id="navbarNav">
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              gap: '15px', // Slightly increase the gap for better spacing
-              padding: '10px', // Add some padding around the container
-            }}
-          >
-            <img
-              src={Logo}
-              style={{
-                width: '60px',
-                height: '62px',
-                borderRadius: '50%',
-                objectFit: 'cover', // Ensure the image covers the container well
-              }}
-              alt="logo"
-            />
-            <h1
-              style={{
-                fontSize: '2rem', // Adjust the size as needed
-                fontWeight: 'bold',
-                margin: '0', // Remove default margin
-                color: '#333', // Use a professional color for the text
-                fontFamily: 'Arial, sans-serif', // Use a professional font
-              }}
-            >
-              Maya
-            </h1>
+      <nav className="navbar navbar-expand-lg navbar-light sticky-top">
+        <div className="navbar-container">
+          <div className="logo-container">
+            <img src={Logo} alt="logo" />
+            <h1>Maya</h1>
           </div>
-
-
-          {isLogin &&
-            <Tabs
-              value={currentTab}
-              onChange={handleTabChange}
-              sx={{
-                marginTop: "10px",
-                marginBottom: "10px",
-                "& .MuiTabs-flexContainer": {
-                  display: "flex",
-                  flexDirection: "row",
-                },
-                "& .MuiTab-root": {
-                  textTransform: "none",
-                  fontSize: "16px",
-                  fontWeight: "400",
-                  lineHeight: "24px",
-                  fontFamily: "Poppins",
-                  color: "#242424",
-                  margin: "4px",
-                  padding: "4px 10px",
-                  ":hover": {
-                    background: "#E6EDF5",
-                  },
-                },
-                "& .Mui-selected": {
-                  fontWeight: 700,
-                },
-                svg: {
-                  width: 16,
-                  height: 16,
-                },
-              }}
-            >
-              <Tab label="Text View" />
-              <Tab label="Graphical View" />
-            </Tabs>
-          }
-        </div>
-        <div
-          style={{
-            display: "flex",
-            gap: "20px",
-            justifyContent: "center",
-            alignItems: "center",
-            paddingRight: "10px",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              gap: "20px",
-              justifyContent: "center",
-              alignItems: "center",
-              marginTop: "10px",
-            }}
-          >
+          <button className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+            <MenuIcon />
+          </button>
+          <div className={`menu-items ${menuOpen ? "active" : ""}`}>
             {isLogin && (
-              <p
-                style={{ cursor: "pointer", "&:hover": { color: "blue" } }}
-                onClick={() => {
-                  navigate("/start-design");
+              <Tabs
+                value={currentTab}
+                onChange={handleTabChange}
+                sx={{
+                  marginTop: "10px",
+                  marginBottom: "10px",
+                  "& .MuiTabs-flexContainer": {
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "baseline",
+                  },
+                  "& .MuiTab-root": {
+                    textTransform: "none",
+                    fontSize: "16px",
+                    fontWeight: "400",
+                    lineHeight: "24px",
+                    fontFamily: "Poppins",
+                    color: "#242424",
+                    margin: "4px",
+                    padding: "4px 10px",
+                    ":hover": {
+                      background: "#E6EDF5",
+                    },
+                  },
+                  "& .Mui-selected": {
+                    fontWeight: 700,
+                  },
+                  svg: {
+                    width: 16,
+                    height: 16,
+                  },
                 }}
               >
-                Start Designing
-              </p>
+                <Tab label="Text View" />
+                <Tab label="Graphical View" />
+              </Tabs>
             )}
-            <p
-              style={{ cursor: "pointer", "&:hover": { color: "blue" } }}
-              onClick={() => {
-                navigate("/pricing");
-              }}
-            >
-              Pricing
-            </p>
-          </div>
-          {isLogin && (
-            <div className="dropdown" ref={dropdownRef}>
-              <button className="dropdown-toggle" onClick={toggleDropdown}>
-                <img
-                  src={avatar}
-                  style={{ width: "32px", marginRight: "5px" }}
-                  alt="avatar"
-                />
-                <span>
-                  {userData?.length > 0
-                    ? userData[0]?.replace("_", " ")
-                    : "Admin"}
-                </span>
-              </button>
-              {isDropdownOpen && (
-                <div className="dropdown33" style={{ minWidth: '280px' }}>
-                  {userData?.length > 1 && < div className="" style={{ cursor: 'default', padding: '5px', paddingLeft: '10px', fontWeight: 600 }}>{userData[0]?.replace('_', " ")}</div>
-                  }
-                  {userData?.length > 1 && <div className="" style={{ cursor: 'default', padding: '5px', paddingLeft: '10px' }}>{userData[3] || "admin@gmail.com"}</div>
-                  }
-                  {userData?.length > 1 && <div className="dropdown-item" style={{ fontWeight: 600 }} onClick={() => navigate('/billing')}>Account & Billing</div>
-                  }
-                  {userData?.length > 1 &&
-                    <div className="" style={{ cursor: 'default', padding: '5px', paddingLeft: '10px', fontWeight: 600 }}>{userData[2]} credits</div>
-                  }                  <hr />
-                  <div className="dropdown-item" onClick={handleLogout}>Logout</div>
+            <div className="menu-options">
+              {isLogin && (
+                <p
+                  onClick={() => {
+                    navigate("/start-design");
+                  }}
+                >
+                  Start Designing
+                </p>
+              )}
+              <p
+                onClick={() => {
+                  navigate("/pricing");
+                }}
+              >
+                Pricing
+              </p>
+              {isLogin && (
+                <div className="dropdown" ref={dropdownRef}>
+                  <button className="dropdown-toggle" onClick={toggleDropdown}>
+                    <img
+                      src={avatar}
+                      alt="avatar"
+                      style={{
+                        width: "2.5rem",
+                        marginRight: ".2rem",
+                      }}
+                      className="img"
+                    />
+                    <span>
+                      {userData?.length > 0
+                        ? userData[0]?.replace("_", " ")
+                        : "Admin"}
+                    </span>
+                  </button>
+                  {isDropdownOpen && (
+                    <div className="dropdown33">
+                      {userData?.length > 1 && (
+                        <div className="dropdown-item">
+                          {userData[0]?.replace("_", " ")}
+                        </div>
+                      )}
+                      {userData?.length > 1 && (
+                        <div className="dropdown-item">
+                          {userData[3] || "admin@gmail.com"}
+                        </div>
+                      )}
+                      {userData?.length > 1 && (
+                        <div
+                          className="dropdown-item"
+                          onClick={() => navigate("/billing")}
+                        >
+                          Account & Billing
+                        </div>
+                      )}
+                      {userData?.length > 1 && (
+                        <div className="dropdown-item">
+                          {userData[2]} credits
+                        </div>
+                      )}
+                      <hr />
+                      <div className="dropdown-item" onClick={handleLogout}>
+                        Logout
+                      </div>
+                    </div>
+                  )}
                 </div>
               )}
+              {!isLogin && (
+                <button className="btn" onClick={handleLogout}>
+                  Login
+                </button>
+              )}
             </div>
-          )}
-          {!isLogin && (
-            <button
-              className="btn"
-              style={{
-                padding: "8px 10px",
-                background: "rgb(72, 136, 200)",
-                width: "100px",
-                borderRadius: "15px",
-                color: "white",
-                border: "none",
-              }}
-              onClick={() => handleLogout()}
-            >
-              Login
-            </button>
-          )}
+          </div>
         </div>
-      </nav >
+      </nav>
     </>
   );
 }
